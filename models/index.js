3cc41d13f69c15e4e54d9c0235fe2075c583fe66
Carmela <carmela881@outlook.com>
2023-04-30 12:14:44 +0100

    defined relationshipo between the four models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+// Define relationship between the four models usign Sequelize ORM
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -5,12 +6,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) - Defines many-to-many relationship
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey:'tag_id'
+});
 
 module.exports = {
   Product,
